90803897998b02df705a40e2e657c515cfd034b5
Erik Enden <20126018+ErikEnden@users.noreply.github.com>
2017-12-04 09:26:17 +0200

    Gallery -> General

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<title>Kooliprojekt </title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="styles/gallery.css" >
+	<link rel="stylesheet" type="text/css" href="styles/general.css" >
 </head>
 <body>
 <nav class="navbar navbar-expand-sm navbar-light bg-light">
