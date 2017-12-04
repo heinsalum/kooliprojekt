1f2eed341ab93f03b09dbf022df250f41c14830c
Markus Heinsalu <heinsalum@users.noreply.github.com>
2017-12-04 10:02:14 +0200

    header

--- header.php
+++ header.php
@@ -2,19 +2,18 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Kooliprojekt </title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="styles/general.css" >
 </head>
 <body>
 <nav class="navbar navbar-expand-sm navbar-light bg-light">
-	<a class="navbar-brand" href="pealeht.php">wao</a>
+	<a class="navbar-brand" href="pealeht.php">Pildigalerii projekt</a>
 	<ul class="nav">
 		<li class="nav-item">
-			<a class="nav-link" href="photoupload.php">Lisa pilte</a>
+			<button type="button" class="btn btn-warning"> <a class="nav-link" href="photoupload.php">Lisa pilte</a></button>
 		</li> 
 		<li class="nav-item">
-			<a class="nav-link" href="?logout=1">Logi välja</a>
+			 <button type="button" class="btn btn-warning"> <a class="nav-link" href="?logout=1">Logi välja</a></button>
 		</li>
 	</ul>
 </nav>
