7c59bb052331aeed29d6d892d91841e17f3a7f30
Kristo Roots <rootkris@users.noreply.github.com>
2017-12-10 20:44:49 +0200

    proov

--- .php
+++ .php
@@ -2,7 +2,55 @@
 	require("functions.php");
 	$notice = "";
 	
+$target_Folder = "pictures/";
 
+$uid = $_POST['id'];
+
+$target_Path = $target_Folder.basename( $_FILES['uploadimage']['name'] );
+
+$savepath = $target_Path.basename( $_FILES['uploadimage']['name'] );
+
+    $file_name = $_FILES['uploadimage']['name'];
+
+    if(file_exists('upload/'.$file_name))
+{
+    echo "That File Already Exisit";
+    }
+    else
+    {
+
+        // Database 
+    $con = mysqli_connect("localhost","if17","if17","if17_rootkris.galleries");
+
+//Check Connection
+        if(mysqli_connect_error())
+        {
+            echo "Ühendus andmebaasiga ebaõnnestus" .     mysqli_connect_error();
+        }
+
+        $sql = "INSERT INTO galleries (name, uploader, filename) VALUES ('','($_SESSION["userId"])','') ";
+
+        if (!mysqli_query($con,$sql))
+        {
+            die('Error: ' . mysqli_error($con));
+        }
+        echo "1 record added successfully in the database";
+        echo '<br />';
+        mysqli_close($con);
+
+        // Faili liigutamine uploaded kausta
+        move_uploaded_file( $_FILES['uploadimage']['tmp_name'],     $target_Path );
+        echo "File Uploaded <br />";
+        echo 'File Successfully Uploaded to:&nbsp;' . $target_Path;
+        echo '<br />';  
+        echo 'File Name:&nbsp;' . $_FILES['uploadimage']['name'];
+        echo'<br />';
+        echo 'File Type:&nbsp;' . $_FILES['uploadimage']['type'];
+        echo'<br />';
+        echo 'File Size:&nbsp;' . $_FILES['uploadimage']['size'];
+
+    }
+	
 	//kui pole sisseloginud, siis sisselogimise lehele
 	if(!isset($_SESSION["userId"])){
 		header("Location: login.php");
